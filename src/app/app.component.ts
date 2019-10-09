28590c577a96f379383264e71ab5760b8e1eb458
ImsirovicAjdin <ajdika@live.com>
2019-10-09 11:54:44 +0200

    build 1 observable with Observable constructor, another with the of() function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
 
+import {Observable} from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html'
 })
-export class AppComponent  {}
+export class AppComponent  {
+
+  // build an observable called appleStream (works, but not the norm)
+  public appleStream = new Observable(appleObserver => {
+    appleObserver.next('Apple 1');
+    appleObserver.next('Apple 2');
+    appleObserver.complete();
+  })
+
+  // build an observable the accepted way
+  public orangeStream = of('Orange 1', 'Orange 2');
+
+  constructor() {
+
+    // subscribe to appleStream observable
+    this.appleStream.subscribe((val) => console.log(val));
+
+    // subscribe to orangeStream Observable
+    this.orangeStream.subscribe((val) => console.log(val));
+  }
+}
